2efe689d8fdb8a11d077c756d961fac2560d2222
Daniel Tanaka <danielh.f.tanaka@gmail.com>
2021-12-12 19:21:22 -0300

    Improve readability

--- AdventOfCode2021/Day1/DayOne.cs
+++ AdventOfCode2021/Day1/DayOne.cs
@@ -7,15 +7,11 @@ namespace AdventOfCode2021.Day1
         public static int GetIncreasingMeasurementsQuantity(IReadOnlyList<int> measurements)
         {
             var increasingMeasurements = 0;
-            for (var currentIndex = 0; currentIndex < measurements.Count; currentIndex++)
+            var containsPreviousToCompareWith = 1;
+            for (var currentIndex = containsPreviousToCompareWith; currentIndex < measurements.Count; currentIndex++)
             {
-                if (currentIndex <= 0)
-                {
-                    continue;
-                }
-
-                var previousMeasurementIndex = currentIndex - 1;
                 var currentMeasurement = measurements[currentIndex];
+                var previousMeasurementIndex = currentIndex - 1;
                 var previousMeasurement = measurements[previousMeasurementIndex];
                 if (currentMeasurement > previousMeasurement)
                 {
